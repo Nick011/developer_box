c082e83dc4b66778aa88b4980c775d9a8d099ee7
Nick <nhagianis@gmail.com>
2013-12-06 16:09:09 -0500

    testing some shit

--- developer_box/models/item.py
+++ developer_box/models/item.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from tag import Tag
 
 class Item(models.Model):
-	user = models.ForeignKey(User)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL)
 	tag = models.ManyToManyField(Tag)
 	title = models.CharField(max_length=75, blank=False, null=False)
 	body = models.TextField(blank=False, null=False)
