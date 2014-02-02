b59827c1adc059dfe02f46dcbf61cd6ee6121164
Nick <nhagianis@gmail.com>
2014-02-01 23:36:42 -0500

    update wsgi to point project root at developer_box folder

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = '/'.join([os.path.dirname(os.path.abspath(__file__)), 'developer_box')
 #settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
 settings_module= "developer_box/settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
