880b4c7000bc73064e6d45f0fb671ad098f90206
Nick <nhagianis@gmail.com>
2014-02-02 01:37:53 -0500

    add default file storage to settings

--- developer_box/settings.py
+++ developer_box/settings.py
@@ -117,6 +117,7 @@ if not DEBUG:
   AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
   AWS_QUERYSTRING_AUTH = True
   AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+  DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
   STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
   S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
   STATIC_URL = S3_URL
