b110e1294932db1caaf573fc1af56678fb66c5df
Nick <nhagianis@gmail.com>
2014-02-01 23:46:37 -0500

    fixed wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import os
 
-PROJECT_ROOT = '/'.join([os.path.dirname(os.path.abspath(__file__)), 'developer_box')
+PROJECT_ROOT = '/'.join([os.path.dirname(os.path.abspath(__file__)), 'developer_box'])
 #settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
-settings_module= "developer_box/settings"
+settings_module= "developer_box.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 from django.core.wsgi import get_wsgi_application
