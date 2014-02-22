1464fe76f16bc561aa8b808b7d87510bec94b310
Nick <nhagianis@gmail.com>
2014-02-22 00:42:22 -0500

    activated add to box feature

--- developer_box/views/item/add.py
+++ developer_box/views/item/add.py
@@ -8,6 +8,6 @@ class ItemAddView(LoginRequired, View):
 	def post(self, request):
 		bucket =	Bucket.objects.get(id=request.POST.get('bucket_id'))
 		item = Item.objects.get(id=request.POST.get('item_id'))
-		bucket.item.add(item)
+		bucket.item_set.add(item)
 		bucket.save()
-		return redirect(reverse('item-detail', kwargs={pk: item.id, slug: item.slug}))
+		return redirect(reverse('item-detail', kwargs={'pk': item.id, 'slug': item.slug}))
--- developer_box/views/profile/detail.py
+++ developer_box/views/profile/detail.py
@@ -18,6 +18,7 @@ class ProfileDetailView(DetailView):
 		context = super(ProfileDetailView, self).get_context_data(**kwargs)
 		user = context['profile'].user
 		query = self.request.GET.get('q', None)
+		buckets = user.bucket_set.all()
 
 		#user with multiple buckets of same slug needs to be handled here!
 		if bucket_slug and query:
@@ -25,11 +26,11 @@ class ProfileDetailView(DetailView):
 		elif query:
 			context['user_items'] = Item.objects.filter(Q(title__contains=query) | Q(description__contains=query))
 		elif bucket_slug:
-			context['user_items'] = Item.objects.filter(bucket__slug=bucket_slug, user=user)
+			context['user_items'] = Item.objects.filter(bucket__slug=bucket_slug, bucket__user=user)
 		else:
-			context['user_items'] = user.item_set.all()
+			context['user_items'] = Item.objects.filter(bucket__user=user)
 
-		context['buckets'] = user.bucket_set.all()
+		context['buckets'] = buckets
 		context['recently_created'] = Item.objects.order_by('created_at').select_related('tag')[:20]
 		context['follower_count'] = Follower.objects.filter(user=user).count()
 		context['following_count'] = Follower.objects.filter(following=user).count()
