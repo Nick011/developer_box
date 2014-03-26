fc17cfe94c21781e50664783343c8788bc6ab3b7
Nick <nhagianis@gmail.com>
2014-03-26 18:34:01 -0400

    updated jquery

--- developer_box/models/bucket.py
+++ developer_box/models/bucket.py
@@ -4,7 +4,7 @@ from django.template.defaultfilters import slugify
 
 class Bucket(models.Model):
 	user = models.ForeignKey(User)
-	title = models.CharField(blank=False, null=False, max_length=75)
+	title = models.CharField(max_length=75)
 	slug = models.SlugField(blank=False, null=False, default=slugify(title))
 	created_at = models.DateTimeField(auto_now=True)
 
